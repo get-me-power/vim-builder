6cd89590c832289d9d477d240fce26a08385f492
kazukazuinaina <ypa.ypa.ypa.t@gmail.com>
2021-05-08 10:57:09 +0900

    fix test case

--- vim_builder/vim_installer_test.go
+++ vim_builder/vim_installer_test.go
@@ -8,7 +8,7 @@ import (
 
 func Test_vim_installed(t *testing.T) {
 	vim_install("$HOME/vim")
-	if f, err := os.Stat("$HOME/vi"); os.IsNotExist(err) || !f.IsDir() {
+	if f, err := os.Stat("$HOME/vim"); os.IsNotExist(err) || !f.IsDir() {
 		t.Errorf("Vim's dir is not found")
 	} else {
 		t.Log("Vim's dir is found")
