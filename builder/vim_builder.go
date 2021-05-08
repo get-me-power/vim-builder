7bdb4f737de2e7fa586976d7e8078fe0c883cc3d
kazukazuinaina <ypa.ypa.ypa.t@gmail.com>
2021-05-08 14:44:16 +0900

    add test and builder

--- builder/vim_builder.go
+++ builder/vim_builder.go
@@ -3,8 +3,32 @@ package builder
 import "os/exec"
 
 func vim_build(buildFlags []string, vimPath string) bool {
-	err := exec.Command("cd", vimPath)
-	if err == nil {
+	err := exec.Command("cd", vimPath+"src").Run()
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func run_configure(buildFlags []string) bool {
+	err := exec.Command("./configure").Run()
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func run_make() bool {
+	err := exec.Command("make")
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func run_make_install() bool {
+	err := exec.Command("make", "install")
+	if err != nil {
 		return false
 	}
 	return true
--- builder/vim_builder_test.go
+++ builder/vim_builder_test.go
@@ -6,7 +6,15 @@ import (
 
 func Test_vim_build(t *testing.T) {
 	if !vim_build([]string{"hoge"}, "$HOME/vim") {
-		t.Error("あほしね")
+		t.Error("cd失敗")
+		return
+	}
+	t.Log("Success")
+}
+
+func Test_run_configure(t *testing.T) {
+	if !run_configure([]string{}) {
+		t.Error("make失敗")
 		return
 	}
 	t.Log("Success")
